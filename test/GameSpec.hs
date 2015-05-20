c460bf09d4f1a69ab11122ad1a502a8284400673
Sven Tennie <sven.tennie@gmail.com>
2015-05-20 14:30:12 +0200

    add some integrations tests

--- test/GameSpec.hs
+++ test/GameSpec.hs
@@ -4,12 +4,23 @@ import           Test.Hspec
 
 spec::Spec
 spec = describe "next generation" $ do
-  it "has no living cells, when there were none in the previous generation" $ do
+  it "has no living cells, when there were none" $ do
     Game.nextGeneration [[0,0,0],[0,0,0],[0,0,0]] `shouldBe` [[0,0,0],[0,0,0],[0,0,0]]
-  it "has no living cells, when there was only one in the previous generation" $ do
+  it "has no living cells, when there was only one" $ do
     Game.nextGeneration [[1,0,0],[0,0,0],[0,0,0]] `shouldBe` [[0,0,0],[0,0,0],[0,0,0]]
-  it "has no living cells, when there were only two neighbours in the previous generation" $ do
+  it "has no living cells, when there were only two neighbours" $ do
     Game.nextGeneration [[1,1,0],[0,0,0],[0,0,0]] `shouldBe` [[0,0,0],[0,0,0],[0,0,0]]
 
-  it "has one living cell, when there were three neigbours in the previous generation" $ do
+  it "has one living cell, when there were three neigbours in a row (top)" $ do
     Game.nextGeneration [[1,1,1],[0,0,0],[0,0,0]] `shouldBe` [[0,1,0],[0,0,0],[0,0,0]]
+  it "has one living cell, when there were three neigbours in a row (middle)" $ do
+    Game.nextGeneration [[0,0,0],[1,1,1],[0,0,0]] `shouldBe` [[0,0,0],[0,1,0],[0,0,0]]
+  it "has one living cell, when there were three neigbours in a row (buttom)" $ do
+    Game.nextGeneration [[0,0,0],[0,0,0],[1,1,1]] `shouldBe` [[0,0,0],[0,0,0],[0,1,0]]
+
+  it "has one living cell, when there were three neigbours in a column (left)" $ do
+    Game.nextGeneration [[1,0,0],[1,0,0],[1,0,0]] `shouldBe` [[0,0,0],[1,0,0],[0,0,0]]
+  it "has one living cell, when there were three neigbours in a column (middle)" $ do
+    Game.nextGeneration [[0,1,0],[0,1,0],[0,1,0]] `shouldBe` [[0,0,0],[0,1,0],[0,0,0]]
+  it "has one living cell, when there were three neigbours in a column (buttom)" $ do
+    Game.nextGeneration [[0,0,1],[0,0,1],[0,0,1]] `shouldBe` [[0,0,0],[0,0,1],[0,0,0]]
