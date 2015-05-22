e1f47dbc5d432ea14e9f7581a397b18e1a22d586
Sven Tennie <sven.tennie@gmail.com>
2015-05-22 10:41:03 +0200

    add print functions

--- src/Main.hs
+++ src/Main.hs
@@ -2,3 +2,16 @@ module Main where
 
 main :: IO ()
 main = putStr "Foo Bar"
+
+printBoard :: [[Int]] -> IO()
+printBoard [] = return()
+printBoard (r:rows) = do printRow r
+                         putChar '\n'
+                         printBoard rows
+
+printRow :: [Int] -> IO()
+printRow [] = return()
+printRow (c:cells) = do case c of
+                         1 -> putChar '*'
+                         0 -> putChar '.'
+                        printRow cells
